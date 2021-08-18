7f404f15e65d9af11cace6fff697cb21e075e474
adam-mehdi <62762546+adam-mehdi@users.noreply.github.com>
2021-08-18 14:08:32 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,27 @@
 
 from setuptools import setup, find_packages
 
-setup(
-    name='FastTimeSformer',
-    version='0.0.1',
-    description='TimeSformer accelerated with linearly-scaling attention',
-    author='',
-    author_email='',
-    url='https://github.com/adam-mehdi/FastTimeSformer.git',
-    packages=find_packages(),
-)
 
+  
+from setuptools import setup, find_packages
+
+setup(
+  name = 'FastTimeSformer',
+  packages = find_packages(),
+  version = '0.0.1',
+  license='MIT',
+  description = 'TimeSformer accelerated with linearly-scaling attention',
+  author = 'Adam Mehdi',
+  author_email = 'adam.mehdi23@gmail.com',
+  url = 'https://github.com/adam-mehdi/FastTimeSformer.git',
+  keywords = [
+    'artificial intelligence',
+    'attention mechanism',
+    'transformers',
+    'video classification',
+  ],
+  install_requires=[
+    'einops>=0.3',
+    'torch>=1.6'
+  ],
+)
