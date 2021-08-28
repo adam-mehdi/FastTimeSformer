1ac918931de77d55881dedd2d7785014905b89fb
adam-mehdi <62762546+adam-mehdi@users.noreply.github.com>
2021-08-28 13:37:55 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'fast_timesformer',
   packages = find_packages(),
-  version = '0.1.0',
+  version = '0.1.1',
   license='MIT',
   description = 'TimeSformer accelerated with linearly-scaling attention',
   author = 'Adam Mehdi',
